176695ae39bb234b1807e89358b66cf4716a32b8
Debora Serra <debora.r.serra@gmail.com>
2025-03-06 09:15:24 -0800

    feat: update db connection to use config

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -1,19 +1,9 @@
-import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import config from './config';
 
-// load environment from the correct .env file based on the NODE_ENV variable (dev, prod, test)
-dotenv.config({ path: `./.env.${process.env.NODE_ENV ?? 'dev'}` });
+const { database, host, password, port, username } = config;
 
-const requiredEnvVars = ['DB_HOST', 'DB_PORT', 'DB_NAME', 'DB_USERNAME', 'DB_PASSWORD'];
-const missingEnvVars = requiredEnvVars.filter((varName) => !process.env[varName]);
-
-if (missingEnvVars.length > 0) {
-  throw new Error(`Missing required environment variables: ${missingEnvVars.join(', ')}`);
-}
-
-const { DB_PORT, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
-
-const MONGO_URI = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+const MONGO_URI = `mongodb://${username}:${password}@${host}:${port}/${database}?authSource=admin`;
 
 const connection = async () => mongoose.connect(MONGO_URI);
 
